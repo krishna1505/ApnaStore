c2b2de3ab05ee38c1734be402ad5a73a0de83922
Krishna Mohan Yadav <145401206+krishna1505@users.noreply.github.com>
2026-05-11 16:42:36 +0530

    Add new CORS origin for admin frontend

--- Server/server.js
+++ Server/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cors({
   origin: [
     "https://apna-store-front.vercel.app",
-    "https://apna-store-admin.vercel.app"
+    "https://apna-store-admin-one.vercel.app"
   ],
   credentials: true
 }));
